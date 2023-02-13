edfd4f38382b0337b3b13f5b8894f8b3f9bcd3ea
MasterDev <juanpb.96@gmail.com>
2023-02-12 20:45:28 -0500

    Add storybook

--- src/DictionaryWebApp.tsx
+++ src/DictionaryWebApp.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+// TODO: Continue reading https://storybook.js.org/tutorials/intro-to-storybook/react/en/simple-component/
+
 const Button = styled.button`
     background-color: red;
 `;
