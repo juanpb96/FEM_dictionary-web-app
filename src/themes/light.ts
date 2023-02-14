cf1f831afb56e4a1b306bfb7bcfb6e7c88e969e3
MasterDev <juanpb.96@gmail.com>
2023-02-14 18:17:45 -0500

    Add light theme color variables

---
+++ src/themes/light.ts
@@ -0,0 +1,14 @@
+export const lightTheme = {
+  colors: {
+    night: '#050505',
+    darkJungle: '#1F1F1F',
+    heavyMetal: '#2D2D2D',
+    charcoal: '#3A3A3A',
+    paleSky: '#757575',
+    mercury: '#E9E9E9',
+    whiteSmoke: '#F4F4F4',
+    white: '#FFFFFF',
+    purpleFlower: '#A445ED',
+    orangeSunset: '#FF5252'
+  }
+};
