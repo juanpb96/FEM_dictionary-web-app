39f084f45bb5d7bfb021c585b03dc72b439fb637
MasterDev <juanpb.96@gmail.com>
2023-02-15 15:29:57 -0500

    Add config to avoid jest error when importing font files

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,10 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
-  setupFiles: ['./jest.setup.js']
+  setupFiles: ['./jest.setup.js'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
+    '^.+\\.(js|jsx)?$': 'babel-jest'
+  },
 }
