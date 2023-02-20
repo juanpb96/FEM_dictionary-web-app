4d29610b87b96f1d2893cd0c7ca26348545e065a
MasterDev <juanpb.96@gmail.com>
2023-02-19 21:36:24 -0500

    Add styles for mobile and event listeners in SearchInput

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,20 +1,37 @@
 import { useState } from 'react';
-
+import * as S from './styles/SearchInput.styled';
 
 export const SearchInput = () => {
   const [inputValue, setInputValue] = useState('');
-  const [isErrorMessageEnabled, setIsErrorMessageEnabled] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const [hasFocus, setHasFocus] = useState(false);
+
+  const onInputContainerFocus = (e: React.FocusEvent<HTMLDivElement>) => {
+    (e.currentTarget.firstChild as HTMLElement).focus();
+  }
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
+
+    if (inputValue !== '' && hasError) {
+      setHasError(false);
+    }
   };
+
+  const onFocus = () => {
+    setHasFocus(true);
+  }
+
+  const onBlur = () => {
+    setHasFocus(false);
+  }
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     // TODO: Call api with search term
 
     if (inputValue === '') {
-      setIsErrorMessageEnabled(true);
+      setHasError(true);
 
       return;
     }
@@ -30,16 +47,23 @@ export const SearchInput = () => {
         Type in the word that you want to search
       </label>
 
-      <div>
+      <S.TextField
+        onFocus={onInputContainerFocus}
+        $hasError={hasError}
+        $hasFocus={hasFocus}
+        tabIndex={0}
+      >
         <input
           id="search-input"
           type="text"
           placeholder="Search for any word…"
           value={inputValue}
           onChange={onChange}
+          onFocus={onFocus}
+          onBlur={onBlur}
         />
 
-        <svg
+        <S.SearchIcon
           xmlns="http://www.w3.org/2000/svg"
           width="18"
           height="18"
@@ -54,14 +78,14 @@ export const SearchInput = () => {
             strokeWidth="1.5"
             d="m12.663 12.663 3.887 3.887M1 7.664a6.665 6.665 0 1 0 13.33 0 6.665 6.665 0 0 0-13.33 0Z"
           />
-        </svg>
-      </div>
+        </S.SearchIcon>
+      </S.TextField>
 
       {
-        isErrorMessageEnabled && (
-          <p>
+        hasError && (
+          <S.ErrorMessage>
             Whoops, can’t be empty…
-          </p>
+          </S.ErrorMessage>
         )
       }
     </form>
--- src/components/styles/SearchInput.styled.ts
+++ src/components/styles/SearchInput.styled.ts
@@ -1,3 +1,79 @@
-export {
+import styled, { css } from 'styled-components';
 
+
+const SearchIcon = styled.svg`
+  width: 15.5px;
+  height: 15.5px;
+`;
+
+interface TextFieldProps {
+  $hasError: boolean;
+  $hasFocus: boolean;
+}
+
+const TextField = styled.div<TextFieldProps>`
+  display: flex;
+  place-items: center;
+  gap: 1.5rem;
+  max-height: 3rem;
+  padding: 1rem 1.5rem;
+  background-color: ${({theme}) => theme.colors.whiteSmoke};
+  border: 1px solid ${({theme}) => theme.colors.whiteSmoke};
+  border-radius: 16px;
+
+  ${props => {
+    if (props.$hasError) {
+      return css`
+        border-color: ${({theme}) => theme.colors.orangeSunset};
+      `
+    }
+  }}
+
+  ${props => {
+    if (props.$hasFocus) {
+      return css`
+        border-color: ${({theme}) => theme.colors.purpleFlower};
+      `
+    }
+  }}
+
+  & input {
+    flex-grow: 1;
+    height: 1.5rem;
+    padding: 0;
+    caret-color: ${({theme}) => theme.colors.purpleFlower};
+    font: 700 16px/16.78px inherit;
+    border: none;
+    background-color: transparent;
+    outline: none;
+  }
+
+  @media screen and (min-width: ${({theme}) => theme.screen.sm}) {
+    & {
+      max-height: 4rem;
+    }
+
+    & input {
+      font-size: 1.25rem;
+      line-height: 1.3125rem;
+    }
+  }
+
+  @media screen and (min-width: ${({theme}) => theme.screen.lg}) {
+    & input {
+      line-height: 1.5rem;
+    }
+  }
+`;
+
+const ErrorMessage = styled.p`
+  margin-top: 0.5rem;
+  font: 400 16px/16.78px inherit;
+  color: ${({theme}) => theme.colors.orangeSunset};
+`;
+
+export {
+  SearchIcon,
+  TextField,
+  ErrorMessage
 };
