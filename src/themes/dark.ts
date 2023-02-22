c609a461722a9185196fca27a2e6a185c70562de
MasterDev <juanpb.96@gmail.com>
2023-02-22 17:15:17 -0500

    Move neutral colors to default file

--- src/themes/dark.ts
+++ src/themes/dark.ts
@@ -4,15 +4,7 @@ import { DefaultStyles } from './default';
 export const darkTheme: DefaultTheme = {
   ...DefaultStyles,
   colors: {
-    night: '#050505',
-    darkJungle: '#1F1F1F',
-    heavyMetal: '#2D2D2D',
-    charcoal: '#3A3A3A',
-    paleSky: '#757575',
-    mercury: '#E9E9E9',
-    whiteSmoke: '#F4F4F4',
-    white: '#FFFFFF',
-    purpleFlower: '#A445ED',
+    ...DefaultStyles.colors,
     error: '#FF5252',
     text: '#FFFFFF',
     boxShadow: 'rgba(164, 69, 237, 1)',
--- src/themes/default.ts
+++ src/themes/default.ts
@@ -1,4 +1,15 @@
 export const DefaultStyles = {
+  colors: {
+    night: '#050505',
+    darkJungle: '#1F1F1F',
+    heavyMetal: '#2D2D2D',
+    charcoal: '#3A3A3A',
+    paleSky: '#757575',
+    mercury: '#E9E9E9',
+    whiteSmoke: '#F4F4F4',
+    white: '#FFFFFF',
+    purpleFlower: '#A445ED',
+  },
   fontFamily: {
     Inconsolata: 'Inconsolata, "Courier New", Courier, monospace',
     Inter: 'Inter, Arial, Helvetica, sans-serif',
