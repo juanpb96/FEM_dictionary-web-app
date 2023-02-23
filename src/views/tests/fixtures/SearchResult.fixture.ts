ea6421c9b03c6e805ee7e87f6753c2bcbf356a28
MasterDev <juanpb.96@gmail.com>
2023-02-23 16:28:30 -0500

    Add API response as sample data

---
+++ src/views/tests/fixtures/SearchResult.fixture.ts
@@ -0,0 +1,392 @@
+const VALID_RESPONSE_1_RESULT = [{
+  word: "keyboard",
+  phonetic: "/ˈkiːbɔːd/",
+  phonetics: [
+    {
+      text: "/ˈkiːbɔːd/",
+      audio: ""
+    },
+    {
+      text: "/ˈkiːbɔːd/",
+      audio: ""
+    },
+    {
+      text: "/ˈkibɔɹd/",
+      audio: "https://api.dictionaryapi.dev/media/pronunciations/en/keyboard-us.mp3",
+      sourceUrl: "https://commons.wikimedia.org/w/index.php?curid=1755168",
+      license: {
+        name: "BY-SA 3.0",
+        url: "https://creativecommons.org/licenses/by-sa/3.0"
+      }
+    }
+  ],
+  meanings: [
+    {
+      partOfSpeech: "noun",
+      definitions: [
+        {
+          definition: "(etc.) A set of keys used to operate a typewriter, computer etc.",
+          synonyms: [],
+          antonyms: []
+        },
+        {
+          definition: "A component of many instruments including the piano, organ, and harpsichord consisting of usually black and white keys that cause different tones to be produced when struck.",
+          synonyms: [],
+          antonyms: []
+        },
+        {
+          definition: "A device with keys of a musical keyboard, used to control electronic sound-producing devices which may be built into or separate from the keyboard device.",
+          synonyms: [],
+          antonyms: []
+        }
+      ],
+      synonyms: [
+        "electronic keyboard"
+      ],
+      antonyms: []
+    },
+    {
+      partOfSpeech: "verb",
+      definitions: [
+        {
+          definition: "To type on a computer keyboard.",
+          synonyms: [],
+          antonyms: [],
+          example: "Keyboarding is the part of this job I hate the most."
+        }
+      ],
+      synonyms: [],
+      antonyms: []
+    }
+  ],
+  license: {
+    name: "CC BY-SA 3.0",
+    url: "https://creativecommons.org/licenses/by-sa/3.0"
+  },
+  sourceUrls: [
+    "https://en.wiktionary.org/wiki/keyboard"
+  ]
+}];
+
+const VALID_RESPONSE_MULTIPLE_RESULTS = [{
+    word: "spell",
+    phonetic: "/spɛl/",
+    phonetics: [
+      {
+        text: "/spɛl/",
+        audio: "https://api.dictionaryapi.dev/media/pronunciations/en/spell-au.mp3",
+        sourceUrl: "https://commons.wikimedia.org/w/index.php?curid=75900056",
+        license: {
+          name: "BY-SA 4.0",
+          url: "https://creativecommons.org/licenses/by-sa/4.0"
+        }
+      },
+      {
+        text: "/spɛl/",
+        audio: "https://api.dictionaryapi.dev/media/pronunciations/en/spell-us.mp3",
+        sourceUrl: "https://commons.wikimedia.org/w/index.php?curid=1762772",
+        license: {
+          name: "BY-SA 3.0",
+          url: "https://creativecommons.org/licenses/by-sa/3.0"
+        }
+      }
+    ],
+    meanings: [
+      {
+        partOfSpeech: "noun",
+        definitions: [
+          {
+            definition: "Words or a formula supposed to have magical powers.",
+            synonyms: [
+              "cantrip",
+              "incantation"
+            ],
+            antonyms: [],
+            example: "He cast a spell to cure warts."
+          },
+          {
+            definition: "A magical effect or influence induced by an incantation or formula.",
+            synonyms: [
+              "cantrip"
+            ],
+            antonyms: [],
+            example: "under a spell"
+          },
+          {
+            definition: "Speech, discourse.",
+            synonyms: [],
+            antonyms: []
+          }
+        ],
+        synonyms: [
+          "cantrip",
+          "cantrip",
+          "incantation"
+        ],
+        antonyms: []
+      },
+      {
+        partOfSpeech: "verb",
+        definitions: [
+          {
+            definition: "To put under the influence of a spell; to affect by a spell; to bewitch; to fascinate; to charm.",
+            synonyms: [],
+            antonyms: []
+          }
+        ],
+        synonyms: [],
+        antonyms: []
+      }
+    ],
+    license: {
+      name: "CC BY-SA 3.0",
+      url: "https://creativecommons.org/licenses/by-sa/3.0"
+    },
+    sourceUrls: [
+      "https://en.wiktionary.org/wiki/spell"
+    ]
+  },
+  {
+    word: "spell",
+    phonetic: "/spɛl/",
+    phonetics: [
+      {
+        text: "/spɛl/",
+        audio: "https://api.dictionaryapi.dev/media/pronunciations/en/spell-au.mp3",
+        sourceUrl: "https://commons.wikimedia.org/w/index.php?curid=75900056",
+        license: {
+          name: "BY-SA 4.0",
+          url: "https://creativecommons.org/licenses/by-sa/4.0"
+        }
+      },
+      {
+        text: "/spɛl/",
+        audio: "https://api.dictionaryapi.dev/media/pronunciations/en/spell-us.mp3",
+        sourceUrl: "https://commons.wikimedia.org/w/index.php?curid=1762772",
+        license: {
+          name: "BY-SA 3.0",
+          url: "https://creativecommons.org/licenses/by-sa/3.0"
+        }
+      }
+    ],
+    meanings: [
+      {
+        partOfSpeech: "verb",
+        definitions: [
+          {
+            definition: "To read (something) as though letter by letter; to peruse slowly or with effort.",
+            synonyms: [],
+            antonyms: []
+          },
+          {
+            definition: "(sometimes with “out”) To write or say the letters that form a word or part of a word.",
+            synonyms: [],
+            antonyms: []
+          },
+          {
+            definition: "To be able to write or say the letters that form words.",
+            synonyms: [],
+            antonyms: [],
+            example: "I find it difficult to spell because I'm dyslexic."
+          },
+          {
+            definition: "Of letters: to compose (a word).",
+            synonyms: [],
+            antonyms: [],
+            example: "The letters “a”, “n” and “d” spell “and”."
+          },
+          {
+            definition: "(with “out”) To clarify; to explain in detail.",
+            synonyms: [],
+            antonyms: [],
+            example: "Please spell it out for me."
+          },
+          {
+            definition: "To indicate that (some event) will occur.",
+            synonyms: [],
+            antonyms: [],
+            example: "This spells trouble."
+          },
+          {
+            definition: "To constitute; to measure.",
+            synonyms: [],
+            antonyms: []
+          },
+          {
+            definition: "To speak, to declaim.",
+            synonyms: [],
+            antonyms: []
+          },
+          {
+            definition: "To tell; to relate; to teach.",
+            synonyms: [],
+            antonyms: []
+          }
+        ],
+        synonyms: [
+          "comprise",
+          "forebode",
+          "mean",
+          "signify",
+          "relieve"
+        ],
+        antonyms: []
+      }
+    ],
+    license: {
+      name: "CC BY-SA 3.0",
+      url: "https://creativecommons.org/licenses/by-sa/3.0"
+    },
+    sourceUrls: [
+      "https://en.wiktionary.org/wiki/spell"
+    ]
+  },
+  {
+    word: "spell",
+    phonetic: "/spɛl/",
+    phonetics: [
+      {
+        text: "/spɛl/",
+        audio: "https://api.dictionaryapi.dev/media/pronunciations/en/spell-au.mp3",
+        sourceUrl: "https://commons.wikimedia.org/w/index.php?curid=75900056",
+        license: {
+          name: "BY-SA 4.0",
+          url: "https://creativecommons.org/licenses/by-sa/4.0"
+        }
+      },
+      {
+        text: "/spɛl/",
+        audio: "https://api.dictionaryapi.dev/media/pronunciations/en/spell-us.mp3",
+        sourceUrl: "https://commons.wikimedia.org/w/index.php?curid=1762772",
+        license: {
+          name: "BY-SA 3.0",
+          url: "https://creativecommons.org/licenses/by-sa/3.0"
+        }
+      }
+    ],
+    meanings: [
+      {
+        partOfSpeech: "noun",
+        definitions: [
+          {
+            definition: "A shift (of work); a set of workers responsible for a specific turn of labour.",
+            synonyms: [],
+            antonyms: []
+          },
+          {
+            definition: "A definite period (of work or other activity).",
+            synonyms: [],
+            antonyms: []
+          },
+          {
+            definition: "An indefinite period of time (usually with a qualifier); by extension, a relatively short distance.",
+            synonyms: [],
+            antonyms: []
+          },
+          {
+            definition: "A period of rest; time off.",
+            synonyms: [],
+            antonyms: []
+          },
+          {
+            definition: "A period of illness, or sudden interval of bad spirits, disease etc.",
+            synonyms: [],
+            antonyms: []
+          },
+          {
+            definition: "An uninterrupted series of alternate overs bowled by a single bowler.",
+            synonyms: [],
+            antonyms: []
+          }
+        ],
+        synonyms: [],
+        antonyms: []
+      },
+      {
+        partOfSpeech: "verb",
+        definitions: [
+          {
+            definition: "To work in place of (someone).",
+            synonyms: [],
+            antonyms: [],
+            example: "to spell the helmsman"
+          },
+          {
+            definition: "To rest (someone or something), to give someone or something a rest or break.",
+            synonyms: [],
+            antonyms: [],
+            example: "They spelled the horses and rested in the shade of some trees near a brook."
+          },
+          {
+            definition: "To rest from work for a time.",
+            synonyms: [],
+            antonyms: []
+          }
+        ],
+        synonyms: [],
+        antonyms: []
+      }
+    ],
+    license: {
+      name: "CC BY-SA 3.0",
+      url: "https://creativecommons.org/licenses/by-sa/3.0"
+    },
+    sourceUrls: [
+      "https://en.wiktionary.org/wiki/spell"
+    ]
+  },
+  {
+    word: "spell",
+    phonetic: "/spɛl/",
+    phonetics: [
+      {
+        text: "/spɛl/",
+        audio: "https://api.dictionaryapi.dev/media/pronunciations/en/spell-au.mp3",
+        sourceUrl: "https://commons.wikimedia.org/w/index.php?curid=75900056",
+        license: {
+          name: "BY-SA 4.0",
+          url: "https://creativecommons.org/licenses/by-sa/4.0"
+        }
+      },
+      {
+        text: "/spɛl/",
+        audio: "https://api.dictionaryapi.dev/media/pronunciations/en/spell-us.mp3",
+        sourceUrl: "https://commons.wikimedia.org/w/index.php?curid=1762772",
+        license: {
+          name: "BY-SA 3.0",
+          url: "https://creativecommons.org/licenses/by-sa/3.0"
+        }
+      }
+    ],
+    meanings: [
+      {
+        partOfSpeech: "noun",
+        definitions: [
+          {
+            definition: "A splinter, usually of wood; a spelk.",
+            synonyms: [],
+            antonyms: []
+          },
+          {
+            definition: "The wooden bat in the game of trap ball, or knurr and spell.",
+            synonyms: [],
+            antonyms: []
+          }
+        ],
+        synonyms: [],
+        antonyms: []
+      }
+    ],
+    license: {
+      name: "CC BY-SA 3.0",
+      url: "https://creativecommons.org/licenses/by-sa/3.0"
+    },
+    sourceUrls: [
+      "https://en.wiktionary.org/wiki/spell"
+    ]
+}];
+
+export {
+  VALID_RESPONSE_1_RESULT,
+  VALID_RESPONSE_MULTIPLE_RESULTS
+};
