aae9b027bf7286864ced89c803926cac17887382
MasterDev <juanpb.96@gmail.com>
2023-02-25 20:52:46 -0500

    Add SearchResultView structure and styles for mobile for a single object as a response

--- src/views/SearchResultView.tsx
+++ src/views/SearchResultView.tsx
@@ -1,4 +1,5 @@
 import { Response } from '../utils/interfaces';
+import * as S from './styles/SearchResultView.styled';
 
 export interface SearchResultViewProps {
   data: Response[];
@@ -12,27 +13,45 @@ export const SearchResultView = ({data}: SearchResultViewProps) => {
     meanings,
     sourceUrls
   } = data[0];
-  const { audio } = phonetics[0];  
+  const { audio } = phonetics[2];  
 
   return (
-    <article>
-      <header>
-        <h2>{ word }</h2>
-        <p data-testid="phonetics">{phonetic}</p>
+    <S.ViewContainer>
+      <S.ViewHeader>
+        <S.Title>{ word }</S.Title>
+        <S.Phonetic data-testid="phonetics">{phonetic}</S.Phonetic>
 
-        <div data-testid="audio-file">
+        <S.PlayButton type="button" data-testid="audio-file">
           <audio src={audio}></audio>
-        </div>
-      </header>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="75"
+            height="75"
+            viewBox="0 0 75 75"
+          >
+            <g
+              fill="#A445ED"
+              fillRule="evenodd"
+            >
+              <circle cx="37.5" cy="37.5" r="37.5" opacity=".25"/>
+              <path d="M29 27v21l21-10.5z"/>
+            </g>
+          </svg>
+        </S.PlayButton>
+      </S.ViewHeader>
 
       {/* FIXME: Should be splitted in other component? */}
 
       {
-        meanings.map(({partOfSpeech, definitions, synonyms}) => (
-          <section key={partOfSpeech}>
-            <h3>{partOfSpeech}</h3>
-            <h4>Meaning</h4>
-            <ul>
+        meanings.map(({partOfSpeech, definitions, synonyms, antonyms}) => (
+          <S.POSSection key={partOfSpeech}>
+            <S.POSTitle>
+              {partOfSpeech}
+            </S.POSTitle>
+            <S.POSSubtitle>
+              Meaning
+            </S.POSSubtitle>
+            <S.DefinitionsList>
               {
                 definitions.map(({definition, example}) => (
                   <li key={definition}>
@@ -41,39 +60,84 @@ export const SearchResultView = ({data}: SearchResultViewProps) => {
                   </li>
                 ))
               }
-            </ul>
+            </S.DefinitionsList>
+            {/* TODO: Check antonyms */}
             {
               synonyms.length > 0 && (
-                <>
-                  <h4>Synonyms</h4>
-                  <ul>
+                <S.WordListContainer>
+                  <S.POSSubtitle>
+                    Synonyms
+                  </S.POSSubtitle>
+                  <S.WordList>
                     {
                       synonyms.map(synonym => (
-                        <li key={synonym}>
+                        <S.WordListItem key={synonym}>
                           {synonym}
-                        </li>
+                        </S.WordListItem>
                       ))
                     }
-                  </ul>
-                </>
+                  </S.WordList>
+                </S.WordListContainer>
               )
             }
-          </section>
+
+            {
+              antonyms.length > 0 && (
+                <S.WordListContainer>
+                  <S.POSSubtitle>
+                    Antonyms
+                  </S.POSSubtitle>
+                  <S.WordList>
+                    {
+                      antonyms.map(antonym => (
+                        <S.WordListItem key={antonym}>
+                          {antonym}
+                        </S.WordListItem>
+                      ))
+                    }
+                  </S.WordList>
+                </S.WordListContainer>
+              )
+            }
+          </S.POSSection>
         ))
       }
 
-      <hr />
+      <S.SourceSection>
+        <S.SourceTitle>
+          Source
+        </S.SourceTitle>
+        
+        <S.SourceList>
+          {
+            sourceUrls.map(sourceUrl => (
+              <S.SourceListItem key={sourceUrl}>
+                <a href={sourceUrl} target="_blank">
+                  {sourceUrl}
+                </a>
 
-      <section>
-        <h3>Source</h3>
-        <p>
-          {/* TODO: Add icon */}
-          {/* TODO: Check if there could be more than one source */}
-          <a href={sourceUrls[0]}>
-            {sourceUrls[0]}
-          </a>
-        </p>
-      </section>
-    </article>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="14"
+                  height="14"
+                  viewBox="0 0 14 14"
+                  role="img"
+                >
+                  <title>Opens in a new tab</title>
+                  <path
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="1.5"
+                    d="M6.09 3.545H2.456A1.455 1.455 0 0 0 1 5v6.545A1.455 1.455 0 0 0 2.455 13H9a1.455 1.455 0 0 0 1.455-1.455V7.91m-5.091.727 7.272-7.272m0 0H9m3.636 0V5"
+                  />
+                </svg>
+              </S.SourceListItem>
+            ))
+          }
+        </S.SourceList>
+      </S.SourceSection>
+    </S.ViewContainer>
   );
 };
--- src/views/styles/SearchResultView.styled.ts
+++ src/views/styles/SearchResultView.styled.ts
@@ -1,1 +1,180 @@
-export {};
+import styled, { css } from 'styled-components';
+import { widthCSS } from '../../utils/utils.styled';
+
+const ViewContainer = styled.article`
+  ${widthCSS}
+  margin: 1.5rem auto;
+`;
+
+const ViewHeader = styled.header`
+  display: grid;
+  align-items: center;
+  grid-template-areas: "title    audio"
+                       "phonetic audio";
+`;
+
+const Title = styled.h2(({theme}) => css`
+  grid-area: title;
+  font-weight: 700;
+  font-size: 2rem;
+  font-family: inherit;
+  /* TODO: Add $font flag to render an individual line height  */
+  /* Consider a function with 3 values depending on each font */
+  line-height: 39px;
+`);
+
+const Phonetic = styled.p(({theme}) => css`
+  grid-area: phonetic;
+  color: ${theme.colors.purpleFlower};
+  font: 400 1.125rem/1.5rem ${theme.fontFamily.Inter};
+`);
+
+const PlayButton = styled.button(({theme}) => css`
+  cursor: pointer;
+  grid-area: audio;
+  justify-self: end;
+  width: 3rem;
+  aspect-ratio: 1 / 1;
+  padding: 0;
+  border: none;
+  background-color: unset;
+
+  & svg {
+    height: inherit;
+  }
+`);
+
+const POSSection = styled.section(({theme}) => css`
+  margin-top: 2rem;
+`);
+
+const POSTitle = styled.h3(({theme}) => css`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-bottom: 2rem;
+  font-weight: 700;
+  font-style: italic;
+  font-size: 1.125rem;
+  /* FIXME: Replace with line-height function */
+  line-height: 22px;
+
+  &::after {
+    content: '';
+    display: inline-block;
+    flex-grow: 1;
+    height: 1px;
+    background-color: ${theme.colors.mercury};
+  }
+`);
+
+const POSSubtitle = styled.h4(({theme}) => css`
+  color: ${theme.colors.paleSky};
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+`);
+
+const DefinitionsList = styled.ul(({theme}) => css`
+  display: flex;
+  flex-direction: column;
+  gap: 0.875rem;
+  margin-top: 1.125rem;
+  padding-left: 1rem;
+
+  & li {
+    padding-left: 0.25rem;
+
+    &::marker {
+      /* TODO: Set this color as a default value in theme */
+      color: #8F19E8;
+    }
+  }
+`);
+
+const WordListContainer = styled.div`
+  display: flex;
+  gap: 1.5rem;
+  margin-top: 1.5rem;
+`;
+
+const WordList = styled.ul`
+  padding: 0;
+  list-style-type: none;
+`;
+
+const WordListItem = styled.li(({theme}) => css`
+  color: ${theme.colors.purpleFlower};
+  font-weight: 700;
+  font-size: 1rem;
+  line-height: 19.36px;
+  /* TODO: Get font styles from a function */
+
+  &:not(:first-child) {
+    margin-top: 0.5rem;
+  }
+`);
+
+const SourceSection = styled.section(({theme}) => css`
+  margin-top: 2rem;
+  border-top: 1px solid ${theme.colors.mercury};
+  padding-top: 1.5rem;
+`);
+
+const SourceTitle = styled.h3(({theme}) => css`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 17px;
+  /* TODO: Get font styles from a function */
+
+  color: ${theme.colors.paleSky};
+  text-decoration: underline;
+`);
+
+const SourceList = styled.ul`
+  padding: 0;
+  margin-top: 0.5rem;
+  list-style-type: none;
+`;
+
+const SourceListItem = styled.li(({theme}) => css`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &:not(:first-child) {
+    margin-top: 6px;
+  }
+
+  & a {
+    color: ${theme.colors.text};
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 17px;
+  }
+
+  & svg {
+    width: 0.75rem;
+    height: 0.75rem;
+    color: ${theme.colors.paleSky}
+  }
+`);
+
+export {
+  ViewContainer,
+  ViewHeader,
+  Title,
+  Phonetic,
+  PlayButton,
+  POSSection,
+  POSTitle,
+  POSSubtitle,
+  DefinitionsList,
+  WordListContainer,
+  WordList,
+  WordListItem,
+  SourceSection,
+  SourceTitle,
+  SourceList,
+  SourceListItem
+};
