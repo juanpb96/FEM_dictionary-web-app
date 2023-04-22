da9268bea93786e02c231474d364cc01c604bdeb
MasterDev <juanpb.96@gmail.com>
2023-04-22 18:20:41 -0500

    Add font enum and types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,7 +1,7 @@
-export const Fonts = {
-  sansSerif: 'Sans Serif',
-  serif: 'Serif',
-  mono: 'Mono'
+export enum FontIds {
+  sansSerif = 'Sans Serif',
+  serif = 'Serif',
+  mono = 'Mono'
 };
 
 export const DROPDOWN_CLASS = {
@@ -9,3 +9,7 @@ export const DROPDOWN_CLASS = {
   closing: 'closing',
   closed: 'closed'
 };
+
+export type FontValues = 'Sans Serif' | 'Serif' | 'Mono'; 
+export type FontTypes = typeof FontIds;
+export type KeyOfFont = keyof FontTypes;
