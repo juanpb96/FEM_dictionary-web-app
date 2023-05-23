fce5554a4ba02e89f73883aef022e95a31e007f9
MasterDev <juanpb.96@gmail.com>
2023-05-23 00:22:38 -0500

    Add new stories for SearchResultView to covert different responses

--- src/views/stories/SearchResultView.stories.tsx
+++ src/views/stories/SearchResultView.stories.tsx
@@ -1,7 +1,12 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Response } from '../../utils/interfaces';
 import { SearchResultView } from '../SearchResultView';
-import { VALID_RESPONSE_1_RESULT } from '../tests/fixtures/SearchResult.fixture';
+import {
+  VALID_RESPONSE_1_RESULT,
+  VALID_RESPONSE_WITH_ANTONYMS,
+  VALID_RESPONSE_MULTIPLE_SOURCES,
+  VALID_RESPONSE_MULTIPLE_RESULTS
+} from '../tests/fixtures/SearchResult.fixture';
 
 export default {
   title: 'Views/SearchResultView',
@@ -13,7 +18,22 @@ export default {
 
 const Template: ComponentStory<typeof SearchResultView> = (args) => <SearchResultView {...args} />;
 
-export const SearchResultOneResult = Template.bind({});
-SearchResultOneResult.args = {
+export const DefaultResult = Template.bind({});
+DefaultResult.args = {
   data: VALID_RESPONSE_1_RESULT as Response[]
+};
+
+export const MultipleAntonymsResult = Template.bind({});
+MultipleAntonymsResult.args = {
+  data: VALID_RESPONSE_WITH_ANTONYMS as Response[]
+};
+
+export const MultipleSourcesResult = Template.bind({});
+MultipleSourcesResult.args = {
+  data: VALID_RESPONSE_MULTIPLE_SOURCES as Response[]
+};
+
+export const MultipleResults = Template.bind({});
+MultipleResults.args = {
+  data: VALID_RESPONSE_MULTIPLE_RESULTS as Response[]
 };
