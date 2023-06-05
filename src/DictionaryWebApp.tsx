5a12a70c70d4e14037771c9e9c47b6ddeeb5b983
MasterDev <juanpb.96@gmail.com>
2023-06-04 23:51:05 -0500

    Add DataProvider and ViewRouter

--- src/DictionaryWebApp.tsx
+++ src/DictionaryWebApp.tsx
@@ -1,13 +1,18 @@
 import { GlobalStyle } from './global.styles';
 import { FontProvider, ThemeProvider } from './contexts';
 import { Header } from './components';
+import { ViewRouter } from './views';
+import { DataProvider } from './contexts/DataProvider';
 
 export const DictionaryWebApp = () => {
   return (
     <FontProvider>
       <ThemeProvider>
         <GlobalStyle />
-        <Header />
+        <DataProvider>
+          <Header />
+          <ViewRouter />
+        </DataProvider>
       </ThemeProvider>
     </FontProvider>
   )
